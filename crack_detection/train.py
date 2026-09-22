f394762fd27390e29fcbe20dd73d597310bba0b1
Rijin Rajeevan <rajeevanrijin@gmail.com>
2026-09-23 00:42:14 +0530

    Add YOLO training script for crack detection model

---
+++ crack_detection/train.py
@@ -0,0 +1,24 @@
+# pyrefly: ignore [missing-import]
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("yolov26n-seg.pt")
+
+    model.train(
+        data="data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=8,
+        device=0,          
+        patience=50,
+        workers=0,
+        project="D:/crack_detection",
+        name="BuildScan_SegModel",
+        exist_ok=True,
+        val=True,
+        plots=True,
+        save=True
+    )
+
+if __name__ == "__main__":
+    main()
